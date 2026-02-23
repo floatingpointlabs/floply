d1c4f6f38128096d896daaa60e9b021eb0052d6e
Nilh0use <nils@woodsideai.com>
2026-02-23 17:38:56 -0500

    chore: rename folders

--- src/app/app.py
+++ src/app/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 from src.app.config import configure_page
-from src.app.pages.about_page import render_about_page
-from src.app.pages.cost_estimator_page import render_model_training_cost_estimator_page
+from src.app.page_renderers.about_page import render_about_page
+from src.app.page_renderers.cost_estimator_page import render_model_training_cost_estimator_page
 
 
 def main():
@@ -13,12 +13,11 @@ def main():
     st.markdown("<h1 style='text-align: center;'>Floply</h1>", unsafe_allow_html=True)
 
     # Sidebar navigation
-    st.sidebar.title("Navigation")
     page = st.sidebar.radio(
-        "Select a page:",
-        ["Model Training Cost Estimator", 
-         "About"],
+        "Navigation",
+        ["Model Training Cost Estimator", "About"],
         index=0,
+        label_visibility="visible",
     )
 
     # Page Selector
